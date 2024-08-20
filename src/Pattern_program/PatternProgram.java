be947769894a86e84a9077baa43d02b64815fa52
Dhiraj <dhirajmandal1216@gmail.com>
2024-08-20 23:24:36 +0530

    Simple Pattern Program.
    *  *  *  *
     *  *  *  *
     *  *  *  *
     *  *  *  *
    
    1 2 3 4 5
    1 2 3 4 5
    1 2 3 4 5
    1 2 3 4 5
    
    1 1 1 1
    2 2 2 2
    3 3 3 3
    4 4 4 4
    
    A B C D
    A B C D
    A B C D
    A B C D

--- src/Pattern_program/PatternProgram.java
+++ src/Pattern_program/PatternProgram.java
@@ -6,6 +6,7 @@ public class PatternProgram {
         obj.pattern1(5);
         obj.pattern2(5);
         obj.pattern3(5);
+        obj.pattern4(5);
     }
     void pattern1(int n){
         for (int row=1;row<n;row++){
@@ -31,6 +32,18 @@ public class PatternProgram {
         for (int row=1;row<n;row++){
             for (int col=1;col<n;col++){
                 System.out.print(row+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    void pattern4(int n){
+        for (int row=1;row<n;row++){
+            char ch='A';
+            for (int col=1;col<n;col++){
+                System.out.print(ch+" ");
+                ch=(char)(ch+1);
             }
             System.out.println();
         }
